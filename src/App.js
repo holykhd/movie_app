c1613de0a4325dc8a0cb1d3b6b123a76180fe1d0
holykhd <holykhd@naver.com>
2021-01-12 15:44:03 +0900

    라우터 분리를 위한 기본 작업

--- src/App.js
+++ src/App.js
@@ -1,44 +1,7 @@
-import React from 'react';
-import axios from "axios";
-import Movie from "./components/Movie";
-import './components/Movie.css';
+import React from "react";
 
-class App extends React.Component{
-    state = {
-        isLoading: true,
-        movies: []
-    };
-
-    getMovies = async () => {
-        const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-        this.setState({movies, isLoading: false})
-    }
-
-    componentDidMount() {
-        this.getMovies();
-    }
-
-    render(){
-        const { isLoading, movies } = this.state;
-        return (
-            <div>
-                {isLoading ? <div>
-                    <p style={{display: 'flex', justifyContent:'center', flexDirection: 'center', alignItems:'center',fontWeight:'bold'}}>Data Loading</p>
-                    </div>:
-                    movies.map(movie => (
-                        <Movie
-                            key={movie.id}
-                            id={movie.id}
-                            genres={movie.genres}
-                            title={movie.title}
-                            year={movie.year}
-                            summary={movie.summary}
-                            poster={movie.medium_cover_image}
-                        />
-                ))}
-            </div>
-        );
-    }
+function App(){
+    return <span>Welcome</span>
 }
 
 export default App;
